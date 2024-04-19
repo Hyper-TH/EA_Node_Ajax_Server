4a5cd0efa664cdfd325c1af2c4f86ff34064a298
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-04-19 23:01:13 +0100

    /getProds to get products

--- products/productsRouter.js
+++ products/productsRouter.js
@@ -1,9 +1,45 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import connectDB from '../mongodb.js';
 
 const router = express.Router();
 
-router.get('/getProds', async (req, res) => {
-    res.status(400);
+connectDB();
+
+const ProductSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    sku: { type: Number, required: false },
+    name: { type: String, required: false },
+    type: { type: String, required: false },
+    price: { type: Number, required: false },
+    upc: { type: String, required: false },
+    category: [{
+        id: { type: String, required: true },
+        name: { type: String, required: true }
+    }],
+    shipping: { type: Number, required: false },
+    description: { type: String, required: false },
+    manufacturer: { type: String, required: false },
+    model: { type: String, required: false },
+    url: { type: String, required: false },
+    image: { type: String, required: false } 
+}, { strict: false });
+
+
+const ProductModel = mongoose.model('products', ProductSchema);
+
+
+router.get("/getProds", async (request, response) => {
+    try {
+        const products = await ProductModel.find({});
+        console.log("Products found:", products);
+
+        response.json({ products: products });
+    } catch (error) {
+        response.status(500).send({ error });
+    }
 });
 
+
 export default router;
+
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import productsRouter from './products/productsRouter.js';
-import { connectDB } from './mongodb.js';
 
 const app = express();
 app.use(cors());
@@ -13,8 +12,6 @@ app.use(express.json({ limit: '50mb' }));
 // Increase URL-encoded payload limit 
 app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 
-// MongoDB connection
-connectDB();
 
 // Endpoint for testing
 app.get('/message', (req, res) => {
