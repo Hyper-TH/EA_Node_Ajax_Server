c40aaa30b6ffc75be50af971078e76872e6f07fe
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-04-29 18:48:37 +0100

    User schema

---
+++ mongodb/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { type: String, required: false },
+    type: { type: String, required: false },
+    product: [{
+        id: { type: String, required: false },
+        name: { type: String, required: false }
+    }],
+}, { strict: false });
+
+const UserModel = mongoose.model('users', UserSchema);
+
+export default UserModel;
