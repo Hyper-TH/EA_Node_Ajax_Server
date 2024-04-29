0a84808e4a71e277177ae95f4de662c211d580d7
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-04-29 17:45:06 +0100

    Added login auth

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import productsRouter from './products/productsRouter.js';
 import connectDB from './mongodb/mongodb.js'
+import admin from 'firebase-admin';
+import { firestore, db } from './config/config.js';
+import { getDocs, collection } from 'firebase/firestore';
+
 
 const app = express();
 app.use(cors());
@@ -23,6 +27,72 @@ app.get('/message', (req, res) => {
 
 app.listen(8000, () => {
     console.log(`Server is running on port 8000`);
+});
+
+// Endpoint to get user authentications
+app.get('/login', async (req, res) => {
+    console.log("Entered endpoint /login");
+    try {
+        const usersCollectionRef = collection(db, "users");
+        const { token } = req.headers; 
+        const { user, uid } = req.query;
+
+        // Fetch all documents from the 'users' collection
+        const data = await getDocs(usersCollectionRef);
+
+        // Map over each document, converting it to a JavaScript object
+        // and adding the document ID as a field
+        let filteredData = data.docs.map((doc) => ({
+            ...doc.data(),  // Spread operator to include all document fields
+            id: doc.id,     // Add the document ID as an 'id' field
+        }))
+        .filter((users) => users.id === user);  // Filter the data to only include the user matching the provided 'user' query parameter
+        
+        console.log(filteredData);
+
+        // Check if user does not exist in the users collection
+        if (filteredData.length === 0) {
+            console.log(`User is not in the database`);
+            const data = {
+                medicines: {},
+                type: "standard"
+            }
+
+            // Add the new user to the 'users' collection with 'user' as the document ID
+            await firestore.collection("users").doc(user).set(data);
+
+            // Fetch the user again 
+            filteredData = data.docs.map((doc) => ({
+                ...doc.data(),  // Spread operator to include all document fields
+                id: doc.id,     // Add the document ID as an 'id' field
+            }))
+            .filter((users) => users.id === user);  // Filter the data to only include the user matching the provided 'user' query parameter
+            
+            console.log("User added to the users collection");
+        } else {
+
+            // Attempt to verify the token using Firebase Admin SDK
+            try {
+                // Check if the UID from the verified token matches the provided 'uid' query parameter
+                const authUser = await admin.auth().verifyIdToken(token);
+
+                if (authUser.uid != uid) {
+                    console.log(`403: UIDs don't match`);
+                    return res.sendStatus(403); 
+                }
+
+            } catch (error) {
+                console.log("Unverified Token");
+                return res.sendStatus(401);
+            }
+        }
+
+        // If verification is successful, respond with the user type of the first matched document
+        res.json({ message: filteredData[0].type});
+
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
 });
 
 app.use(productsRouter);
